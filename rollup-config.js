2aba0da3696ba9fd68f0ca1cd96e3b377d1a04c1
Nicholas Jamieson <nicholas@cartant.com>
2017-12-30 11:24:18 +1000

    chore(rollup): Upgrade config.

--- rollup-config.js
+++ rollup-config.js
@@ -1,7 +1,7 @@
 export default {
-    banner: "/*MIT license https://github.com/cartant/firebase-key/blob/master/LICENSE*/",
     input: "dist/index.js",
     output: {
+        banner: "/*MIT license https://github.com/cartant/firebase-key/blob/master/LICENSE*/",
         file: "bundles/firebase-key.umd.js",
         format: "umd",
         name: "firebaseKey"
